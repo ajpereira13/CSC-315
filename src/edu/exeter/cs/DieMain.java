11cac8fe7d73b2c588c749e5c3fbf73c772777d4
ajpereira13 <a.j.pereira13@hotmail.com>
2016-04-22 17:09:50 -0400

    Commit

--- src/edu/exeter/cs/DieMain.java
+++ src/edu/exeter/cs/DieMain.java
@@ -25,6 +25,7 @@ public class DieMain {
 			faceValue = 1;
 		}
 
+		//This function generates a pseudorandom number between 1 and 6.
 		public int roll(){
 			count++;
 			faceValue = (int)(Math.random()*MAX)+1;
@@ -46,14 +47,17 @@ public class DieMain {
 			}
 		}
 		
+		//This function gets the count.
 		public int getRoll() {
 			return count;
 		}
 		
+		//This function sets the count.
 		public void setRoll(int n) {
 			count = n;
 		}
 
+		//This function returns the faceValue of the die a string so it can be printed.
 		public String toString(){
 			String result = Integer.toString(faceValue);
 			return result;
